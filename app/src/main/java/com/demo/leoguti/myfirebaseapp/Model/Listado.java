f827762593b0b3e076cc62f87e7a3b9c5a51e487
caro1853 <caro1853@gmail.com>
2017-07-02 21:12:35 -0500

    Se agregan cosas de consulta la información

---
+++ app/src/main/java/com/demo/leoguti/myfirebaseapp/Model/Listado.java
@@ -0,0 +1,8 @@
+package com.demo.leoguti.myfirebaseapp.Model;
+
+/**
+ * Created by carolina on 02/07/2017.
+ */
+
+public class Listado {
+}
---
+++ app/src/main/java/com/demo/leoguti/myfirebaseapp/Varios/Accion.java
@@ -0,0 +1,8 @@
+package com.demo.leoguti.myfirebaseapp.Varios;
+
+/**
+ * Created by carolina on 02/07/2017.
+ */
+
+public class Accion {
+}
